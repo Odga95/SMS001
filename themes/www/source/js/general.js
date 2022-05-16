0d74f1a428bfb78ce004e09bf6cbd42c3172dcf5
David Burbano <odga95@gmail.com>
2022-05-16 17:01:30 -0500

    slider testimonials - falta media movil2

--- themes/www/source/js/general.js
+++ themes/www/source/js/general.js
@@ -102,7 +102,7 @@ function showDivsT(m) {
     if (m < 1) { slideIndexTestimonial = y.length }
     for (j = 0; j < y.length; j++) {
         
-        y[j].style.cssText += "opacity: 0;display: flex"
+        y[j].style.cssText += "opacity:0; display: flex"
 
     }
     y[slideIndexTestimonial - 1].style.cssText += "opacity:1; display:flex"
