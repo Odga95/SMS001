19ae3a30ca2a372ab036f178c0c5118c371ae271
David Burbano <odga95@gmail.com>
2022-05-18 17:01:05 -0500

    muestras trabajos + texto

--- themes/www/source/js/general.js
+++ themes/www/source/js/general.js
@@ -73,12 +73,17 @@ function currentDiv(n) {
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("mySlide");
+    var y = document.getElementsByClassName("mySlideText");
     if (n > x.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = x.length }
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
     x[slideIndex - 1].style.display = "block";
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    y[slideIndex - 1].style.display = "block";
 }
 
 
